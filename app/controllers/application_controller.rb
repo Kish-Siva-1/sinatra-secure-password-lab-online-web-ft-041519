b9d7bffd10dc3f60fc1878bc133688606e47f1e1
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-10 15:11:42 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
      user = User.find_by(:username => params[:username])
-    if user
+    if user && user.authenticate(params[:password])
       redirect "/account"
     else
       redirect "/failure"
